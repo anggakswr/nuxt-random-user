b1ae0f74384141b7104805d993f2dadb2857c67b
anggakswr <222180023@student.unaki.ac.id>
2022-05-21 14:06:05 +0700

    Added pagination

--- pages/index.vue
+++ pages/index.vue
@@ -15,15 +15,30 @@
       <IndexUserSkeleton />
     </section>
 
+    <!-- data -->
     <section v-else class="grid grid-cols-4 mt-8 gap-4">
       <IndexUser
-        v-for="(user, index) in users"
+        v-for="(user, index) in usersShown"
         :key="'user-' + user.email + index"
         :user="user"
       />
     </section>
 
+    <!-- if there's no data -->
     <p v-if="!loading && !users.length" class="mt-8">No data</p>
+
+    <!-- pagination -->
+    <div class="box-center mt-8 gap-x-4">
+      <button :disabled="cut < 1" @click="prevPage">
+        <span>&larr;</span>
+        <span>Previous Page</span>
+      </button>
+
+      <button :disabled="cut > 23" @click="nextPage">
+        <span>Next Page</span>
+        <span>&rarr;</span>
+      </button>
+    </div>
   </main>
 </template>
 
@@ -35,6 +50,10 @@ export default {
       loading: false,
       error: '',
       users: [],
+      usersShown: [],
+      // pagination
+      cut: 0,
+      until: 4,
     }
   },
   fetch() {
@@ -46,6 +65,20 @@ export default {
     }
   },
   methods: {
+    prevPage() {
+      if (this.cut > 0) {
+        this.usersShown = this.users.slice(this.cut - 4, this.until - 4)
+        this.cut = this.cut - 4
+        this.until = this.until - 4
+      }
+    },
+    nextPage() {
+      if (this.cut < 24) {
+        this.usersShown = this.users.slice(this.cut + 4, this.until + 4)
+        this.cut = this.cut + 4
+        this.until = this.until + 4
+      }
+    },
     async getUsers() {
       this.error = ''
       this.loading = true
@@ -58,6 +91,7 @@ export default {
         })
 
         this.users = res.data.results
+        this.usersShown = res.data.results.slice(this.cut, this.until)
       } catch {
         this.error = 'Sorry, an error occurred while displaying data'
       }
@@ -67,3 +101,10 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+button:disabled {
+  opacity: 0.7;
+  cursor: not-allowed;
+}
+</style>
