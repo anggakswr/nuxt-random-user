c1f87a27e3def59408099af299aaa48d1e524537
anggakswr <222180023@student.unaki.ac.id>
2022-05-21 10:25:45 +0700

    Fetch users data in index page

---
+++ pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <div>emp</div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      loading: false,
+    }
+  },
+  fetch() {
+    this.getUsers()
+  },
+  methods: {
+    async getUsers() {
+      this.loading = true
+
+      try {
+        const res = await this.$axios.get('/', {
+          params: {
+            results: 28,
+          },
+        })
+
+        console.log('get users', res.data)
+      } catch {
+        //
+      }
+
+      this.loading = false
+    },
+  },
+}
+</script>
